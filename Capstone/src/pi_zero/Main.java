0675ab809d2af1bc66a18dc3e5c0d5fa6e2c7622
schesg01 <schesg01@students.ipfw.edu>
2018-03-29 20:44:19 -0400

    Main command line update

--- Capstone/src/pi_zero/Main.java
+++ Capstone/src/pi_zero/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
+		// command line params
 		int sleepTime = Integer.parseInt(args[0]);
 		DataApplication application = new DataApplication(sleepTime);
 		application.start();
