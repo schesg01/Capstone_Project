16ab9a05834037e08f0008ff32664bfeb9f8b470
Garrett <garrett.atoms@gmail.com>
2018-04-17 20:46:07 -0400

    Added crts and csrs and updated scripts.

--- Capstone/compile.sh
+++ Capstone/compile.sh
@@ -1,1 +1,1 @@
-javac -cp "./src:./src/pi_zero:/home/pi/paho/org.eclipse.paho.client.mqttv3-1.2.1-20180313.040548-111.jar" src/pi_zero/Main.java
+javac -cp "./src:./src/pi_zero:/home/pi/paho/org.eclipse.paho.client.mqttv3-1.2.1-20180313.040548-111.jar:bcpkix-jdk15on-159.jar:bcprov-ext-jdk15on-159.jar:bctls_jdk15on-159.jar" src/pi_zero/Main.java
